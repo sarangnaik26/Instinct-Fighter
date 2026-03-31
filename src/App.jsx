45e1aeb9ab25f4cc8ba05aedf6b06b13f3d8e463
Sarang Naik <sarangnaik250306@gmail.com>
2026-03-31 20:14:19 +0530

    chore: revert gh-pages deployment changes, restore local dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import GameSelectPage from './pages/GameSelectPage'
 import PlayPage from './pages/PlayPage'
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className="w-full h-full bg-bg-dark">
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -13,6 +13,6 @@ export default function App() {
           <Route path="/play/:gameId/:mode" element={<PlayPage />} />
         </Routes>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
